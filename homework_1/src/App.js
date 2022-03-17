73c3c869de94d9fb0370b34b92492e148e62397c
Klaatu98 <arabson1998@gmail.com>
2022-03-17 16:14:29 +0400

    homework_1

---
+++ homework_1/src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import Counter from './components/Counter';
+import WeekDays from './components/WeekDays';
+
+
+function App() {
+    const days = [
+        { id: 1, day: 'ორშაბათი' },
+        { id: 2, day: 'სამშაბათი' },
+        { id: 3, day: 'ოთხშაბათი' },
+        { id: 4, day: 'ხუთშაბათი' },
+        { id: 5, day: 'პარასკევი' },
+        { id: 6, day: 'შაბათი' },
+        { id: 7, day: 'კვირა' },
+    ]
+
+    
+
+
+    return (
+        <>
+            <WeekDays days={days}/>
+            <Counter />
+        </>
+    );
+}
+
+export default App;
